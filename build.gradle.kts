2dd1ee0bf03de68a75cd32491d9ba52a1ea381e4
RedGamer6427A <arthur.hurt.11@icloud.com>
2024-09-02 19:57:22 +0200

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ tasks.test {
 
 tasks.jar {
     archiveBaseName.set("RedUtilsTest")
-    archiveVersion.set("1.0.0")
+    archiveVersion.set("1.0.5")
     archiveClassifier.set("") // Leave empty if no classifier is needed
 
     manifest {
@@ -37,26 +37,14 @@ tasks.jar {
 publishing {
     repositories {
         maven {
-            name = "RedUtils"
-            url = uri("https://maven.pkg.github.com/RedGamer6427A/RedUtils")
-
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                username = "RedGamer6427A"
-                password = "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"
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
             }
         }
     }
 
-    publications {
-        create<MavenPublication>("gpr") {
-            from(components["java"])
 
-            // If you have a custom artifact, use this instead:
-            // artifact("build/libs/your-artifact.jar")
-
-            groupId = "dev.redgamer6427a" // Replace with your package's group ID
-            artifactId = "RedUtils" // Replace with your artifact ID
-            version = "1.0.0" // Replace with your package's version
-        }
-    }
 }
